9aceb62f52c895def15c1ba69cbdf8e98ea49565
Leo <danielesquivel00.lded@gmail.com>
2022-08-14 01:11:47 -0500

    ADD new popup module

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,17 @@
 import { encrypt, decrypt } from './encrypt.js';
+import { showPopup } from './popup.js';
 
+//Elementos del encrypt/decrypt
 const btnEncrypt = document.getElementById('encrypt')
 const btnDecrypt = document.getElementById('decrypt');
 
 const textArea = document.getElementById('mensaje');
 const respuesta = document.getElementById('respuesta');
 
+const specialCharactersPattern = /[ `!@#$%^&*()_+\-=\[\]{}';:"\\|,.<>\/?~]/;
+
+
+//Metodos para encrypt/decrypt
 btnEncrypt.onclick = () => {
     tipoAccion(true);
 
@@ -20,7 +26,13 @@ const tipoAccion = ( tipo ) => {
     let mensaje = textArea.value.toLowerCase();
 
     if( !mensaje ) {
-        return console.log('No hay nada');
+        showPopup('No se ha ingresado texto.', 'Por favor escriba algo para poder encriptarlo.');
+        return;
+    }
+
+    if( specialCharactersPattern.test( mensaje )) {
+        showPopup('Caracter no valido', 'No se aceptan los siguientes caracteres: ' + specialCharactersPattern);
+        return console.log('Hay caracter no valido')
     }
 
     mensaje = tipo 
